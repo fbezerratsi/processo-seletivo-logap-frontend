812551217da970fa2756b7c8c380f39449579b66
Felipe Bezerra <fbezerra.tsi@gmail.com>
2021-06-07 19:56:26 -0300

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/frontend'));
+app.use(express.static('./dist/processo-seletivo-logap-fronte'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: '/dist/frontend/'}),
+    res.sendFile('index.html', {root: 'dist/processo-seletivo-logap-fronte/'}),
 );
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8080);
